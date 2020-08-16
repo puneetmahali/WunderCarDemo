7516354656bf607cf9752971ed57d7cf785e25fd
Puneet Mahali <iospuneet@gmail.com>
2020-08-16 04:34:55 +0200

    refactoring

--- WunderDemo/WunderDemo/UI/MapTab/MapTabViewController.swift
+++ WunderDemo/WunderDemo/UI/MapTab/MapTabViewController.swift
@@ -99,7 +99,7 @@ extension MapTabViewController: MKMapViewDelegate{
         var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseID)
         if annotationView == nil {
             annotationView =  MKAnnotationView(annotation: annotation, reuseIdentifier: reuseID)
-            annotationView?.image = UIImage(named: "ic_marker")
+            annotationView?.image = UIImage(systemName: "mappin.circle.fill")
             annotationView?.canShowCallout = true
             annotationView?.annotation = annotation
         }else{
